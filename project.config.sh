3873a6342ecc1083b42dd47dde7a7e8924585b8a
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-23 17:34:48 +0200

    feat: setup gitbackup build

---
+++ project.config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export DIST_DIR="${PROJECT_DIR}/dist"
+export REPOSITORIES_LIST_ENDPOINT="https://raw.githubusercontent.com/Jblew/osowiec-git-backup/master/repositories.lst"
