81da8b0808967947804d1dbb74e8f0b74e3cfa9d
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-23 20:51:24 +0200

    feat: format branches log

--- gitbackup/gitpuller/formatRepoStatus.go
+++ gitbackup/gitpuller/formatRepoStatus.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 )
 
 func formatRepoStatus(repo *git.Repository, name string) string {
@@ -13,8 +14,8 @@ func formatRepoStatus(repo *git.Repository, name string) string {
 	out += "\n"
 	out += formatHead(repo)
 	out += "\n"
-
-	// TODO print status for all remotes
+	out += formatBranches(repo)
+	out += "\n"
 
 	return out
 }
@@ -33,6 +34,21 @@ func formatRemotes(repo *git.Repository) string {
 	return out
 }
 
+func formatBranches(repo *git.Repository) string {
+	out := "  Branches:\n"
+	branchesIter, err := repo.Branches()
+	if err != nil {
+		out += fmt.Sprintf("Cannot get branches: %v\n", err)
+		return out
+	}
+	branchesIter.ForEach(func(branch *plumbing.Reference) error {
+		out += formatRef(repo, branch)
+		return nil
+	})
+
+	return out
+}
+
 func formatHead(repo *git.Repository) string {
 	out := "  Head:\n"
 
@@ -41,14 +57,23 @@ func formatHead(repo *git.Repository) string {
 		out += fmt.Sprintf("Cannot get HEAD ref: %v\n", err)
 		return out
 	}
-	commit, err := repo.CommitObject(headRef.Hash())
+
+	out += formatRef(repo, headRef)
+	out += "\n"
+
+	return out
+}
+
+func formatRef(repo *git.Repository, ref *plumbing.Reference) string {
+	out := fmt.Sprintf("[%s]", ref.Name().String())
+
+	commit, err := repo.CommitObject(ref.Hash())
 	if err != nil {
-		out += fmt.Sprintf("Cannot get HEAD commit by hash: %v\n", err)
+		out += fmt.Sprintf("Cannot get commit by hash: %v\n", err)
 		return out
 	}
 
 	out += fmt.Sprintf("  %s", commit.String())
-	out += "\n"
 
 	return out
 }
