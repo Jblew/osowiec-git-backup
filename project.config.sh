f4afc54a3e0a5593441c17bee9f7390de9bc50f7
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-23 21:24:49 +0200

    ci: missing config variables in project config

--- project.config.sh
+++ project.config.sh
@@ -3,4 +3,9 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export DIST_DIR="${PROJECT_DIR}/dist"
 export REPOSITORIES_LIST_ENDPOINT="https://raw.githubusercontent.com/Jblew/osowiec-git-backup/master/repositories.lst"
-export REPOSITORIES_DIR="./repos"
+export REPOSITORIES_DIR=""
+export SSH_PRIVATE_KEY_PATH=""
+export MONITORING_ENDPOINT_LOG=""
+export MONITORING_ENDPOINT_PING_SUCCESS=""
+export MONITORING_ENDPOINT_PING_FAILURE=""
+# I use https://healthchecks.io/ for ping monitoring
