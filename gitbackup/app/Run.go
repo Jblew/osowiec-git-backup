464c65ec4fb39d1ed814974230dfc93ef0d27174
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-02-17 18:15:01 +0100

    feat: prometheus basic metrics

--- gitbackup/app/Run.go
+++ gitbackup/app/Run.go
@@ -4,27 +4,35 @@ import (
 	"fmt"
 	"gitbackup/util"
 	"log"
-	"net"
+	"time"
 
-	"golang.org/x/crypto/ssh"
+	"github.com/prometheus/client_golang/prometheus"
+	prometheusPush "github.com/prometheus/client_golang/prometheus/push"
 )
 
 // Run runs the app
 func Run(config Config) error {
+	initMetrics()
 	app := App{Config: config}
-	err := app.doPull()
+	runFunc := func() error {
+		return app.doPull()
+	}
+	err := measureTimeMetric(runFunc)
+
 	if err != nil {
+		runsCounter.With(prometheus.Labels{"status": "failure"}).Inc()
 		pingErr := app.pingMonitoringFailure()
 		if pingErr != nil {
-			return fmt.Errorf("Pull failed with [%v] and cannot ping monitoring failure [%v]", err, pingErr)
+			fmt.Printf("Pull failed with [%v] and cannot ping monitoring failure [%v]", err, pingErr)
 		}
-		return err
+	} else {
+		runsCounter.With(prometheus.Labels{"status": "success"}).Inc()
+		pingErr := app.pingMonitoringSuccess()
+		if pingErr != nil {
+			fmt.Printf("Pull succeeded but cannot ping monitoring success [%v]", pingErr)
+		}
 	}
-
-	pingErr := app.pingMonitoringSuccess()
-	if pingErr != nil {
-		return fmt.Errorf("Pull succeeded but cannot ping monitoring success [%v]", pingErr)
-	}
+	pushMetrics(config)
 
 	return nil
 }
@@ -34,13 +42,12 @@ func (app *App) doPull() error {
 	if err != nil {
 		return fmt.Errorf("Cannot load repository list: %v", err)
 	}
+	repositoryCountGauge.Set(float64(len(app.Repositories)))
 
 	auth, err := util.GetSSHPublicKeyFromPrivateKeyFile(app.Config.SSHPrivateKeyPath)
 	if err != nil {
 		return fmt.Errorf("Cannot load ssh public key from private key file: %v", err)
 	}
-
-	auth.HostKeyCallback = MakeEmptyHostkeyCallback()
 	app.Auth = auth
 
 	log.Printf("Repositories: %v", app.Repositories)
@@ -51,10 +58,46 @@ func (app *App) doPull() error {
 	return nil
 }
 
-func MakeEmptyHostkeyCallback() ssh.HostKeyCallback {
-	// allows all known hosts
-	return func(hostname string, remote net.Addr, key ssh.PublicKey) error {
-		return nil
-	}
+var (
+	runsCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "gitbackup_runs_count",
+		Help: "Count of runs of gitbackup",
+	}, []string{"status"})
 
+	timeGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "gitbackup_run_time",
+		Help: "Time of running gitbackup",
+	}, []string{"status"})
+
+	repositoryCountGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "gitbackup_repository_count",
+		Help: "Number of all repositories",
+	})
+)
+
+func initMetrics() {
+	prometheus.MustRegister(runsCounter, timeGauge, repositoryCountGauge)
+}
+
+func pushMetrics(config Config) {
+	if config.PrometheusPushGatewayURL == "" {
+		fmt.Printf("PrometheusPushGatewayURL not configured via env (consult Dockerfile). Skipping prometheus metrics pushing")
+		return
+	}
+	err := prometheusPush.New(config.PrometheusPushGatewayURL, config.PrometheusJobName).Gatherer(prometheus.DefaultGatherer).Push()
+	if err != nil {
+		fmt.Printf("Error while pushing prometheus metrics: %v", err)
+	}
+}
+
+func measureTimeMetric(f func() error) error {
+	start := time.Now()
+	err := f()
+	ellapsedSeconds := time.Since(start)
+	if err != nil {
+		timeGauge.With(prometheus.Labels{"status": "error"}).Set(ellapsedSeconds.Seconds())
+	} else {
+		timeGauge.With(prometheus.Labels{"status": "success"}).Set(ellapsedSeconds.Seconds())
+	}
+	return err
 }
--- gitbackup/go.mod
+++ gitbackup/go.mod
@@ -5,5 +5,6 @@ go 1.14
 require (
 	github.com/go-git/go-billy/v5 v5.0.0
 	github.com/go-git/go-git/v5 v5.1.0
+	github.com/prometheus/client_golang v1.12.1 // indirect
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292
 )
